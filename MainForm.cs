ad86f1ee1db8415b893d01189e5614203ec783af
KwaneleSishi <kwanelesishi050509@gmail.com>
2025-04-06 12:29:50 +0200

    Clean

--- MainForm.cs
+++ MainForm.cs
@@ -73,68 +73,6 @@ namespace IncomeAndExpensesTrackingSystem
             }
         }
 
-        /*
-        private void close_Click(object sender, EventArgs e)
-        {
-            if (MessageBox.Show("Are you sure you close?", "Confirmation Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-            {
-                Application.Exit(); 
-            }
-
-        }
-
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            dashboard1.Visible = false;
-            categoryForm1.Visible = false;
-            incomeForm1.Visible = true;
-            expensesForm1.Visible = false;
-        }
-
-        private void userControl11_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            if(MessageBox.Show("Are you sure you want to logout?", "Confirmation Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-            {
-                Form1 loginForm = new Form1();
-                loginForm.Show();
-
-                this.Hide();
-            }
-        }
-
-        private void dashboard_btn_Click(object sender, EventArgs e)
-        {
-            dashboard1.Visible = true;
-            categoryForm1.Visible = false;
-            incomeForm1.Visible = false;
-            expensesForm1.Visible = false;
-        }
-
-        private void addCategory_btn_Click(object sender, EventArgs e)
-        {
-            dashboard1.Visible = false;
-            categoryForm1.Visible = true;
-            incomeForm1.Visible = false;
-            expensesForm1.Visible = false;
-        }
-
-        private void expenses_btn_Click(object sender, EventArgs e)
-        {
-            dashboard1.Visible = false;
-            categoryForm1.Visible = false;
-            incomeForm1.Visible = false;
-            expensesForm1.Visible = true;
-        }
-        */
+      
     }
 }
